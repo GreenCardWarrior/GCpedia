e7ed857507be2c746dac9d06f9adaa0ba093c9e5
GreenCardWarrior <my2012toyotacamry@gmail.com>
2022-01-06 20:56:35 -0700

    initial commit

---
+++ docs/precache-manifest.5454140d9505eff214ebc6d19131c0a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4de48f2f28915dbf6d1b53f1f4ac5ef",
+    "url": "/GCPedia/index.html"
+  },
+  {
+    "revision": "39712ab9dfe228ad7782",
+    "url": "/GCPedia/static/css/main.0fa6ab41.chunk.css"
+  },
+  {
+    "revision": "8d9a5288f3e2eb3beecf",
+    "url": "/GCPedia/static/js/2.4f3d275d.chunk.js"
+  },
+  {
+    "revision": "39712ab9dfe228ad7782",
+    "url": "/GCPedia/static/js/main.20633306.chunk.js"
+  },
+  {
+    "revision": "4ee42bd7c9aa6b467180",
+    "url": "/GCPedia/static/js/runtime~main.7d2b744e.js"
+  },
+  {
+    "revision": "4a5164aec554743271ff9543ba78b010",
+    "url": "/GCPedia/static/media/arrow-down-mint.4a5164ae.svg"
+  }
+]);
