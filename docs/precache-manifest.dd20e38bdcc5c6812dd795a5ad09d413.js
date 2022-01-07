ab549be1fc05c17f64ce966fc0d308fd1d7e0f20
GreenCardWarrior <my2012toyotacamry@gmail.com>
2022-01-06 21:18:39 -0700

    updated

---
+++ docs/precache-manifest.dd20e38bdcc5c6812dd795a5ad09d413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0104ff35b557ebfbb0df97a686273f",
+    "url": "/gcpedia/index.html"
+  },
+  {
+    "revision": "22ccc6562c52946a9c4c",
+    "url": "/gcpedia/static/css/main.2b712a4b.chunk.css"
+  },
+  {
+    "revision": "52163baf1b83fa25ef02",
+    "url": "/gcpedia/static/js/2.4ab0f7ad.chunk.js"
+  },
+  {
+    "revision": "22ccc6562c52946a9c4c",
+    "url": "/gcpedia/static/js/main.820ab2d3.chunk.js"
+  },
+  {
+    "revision": "39dc472306fb6a7a8932",
+    "url": "/gcpedia/static/js/runtime~main.44507ab9.js"
+  },
+  {
+    "revision": "4a5164aec554743271ff9543ba78b010",
+    "url": "/gcpedia/static/media/arrow-down-mint.4a5164ae.svg"
+  }
+]);
